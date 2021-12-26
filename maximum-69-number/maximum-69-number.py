eb38af8fe1537f3d68d542bdc61c95088cdffda8
Anik Saha <aniksahafp@gmail.com>
2021-12-26 16:43:34 +0530

    Time: 28 ms (82.96%), Space: 14.4 MB (6.13%) - LeetHub

--- maximum-69-number/maximum-69-number.py
+++ maximum-69-number/maximum-69-number.py
@@ -1,5 +1,13 @@
 class Solution:
     def maximum69Number (self, num: int) -> int:
-        return int(str(num).replace('6','9',1))
+        # return int(str(num).replace('6','9',1))
+        i=1000
+        while (i>=1):
+            print(int(num//i))
+            print(int(num//(i*10))*10)
+            if int(num//i)-int((num//(i*10)))*10==6:
+                return int(num+i*3)
+            i/=10
+        return int(num)
         
         

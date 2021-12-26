ee12fc592cadc810c7fc97c3bfdc2a1eb888d290
Anik Saha <aniksahafp@gmail.com>
2021-12-27 04:05:15 +0530

    Time: 116 ms (77.12%), Space: 15 MB (76.16%) - LeetHub

--- count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.py
+++ count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.py
@@ -1,7 +1,8 @@
 class Solution:
     def countNegatives(self, grid: List[List[int]]) -> int:
-        n=len(grid[0])
-        globalcount=0
+#         Solution 1
+#         n=len(grid[0])
+#         globalcount=0
 #         for row in grid:
 #             left=0
 #             right=n-1
@@ -16,20 +17,29 @@ class Solution:
 #             if left<=n:
 #                 globalcount+=(n-left)
 #         return globalcount
-        left=0
-
-        for row in reversed(grid):
-            right=n-1
-            # print("lr",left," ",right," ",row)
-            while(left<=right):
-                mid=left+(right-left)//2
-                # print(row[mid])
-                if(row[mid]>-1):
-                    left=mid+1
-                else:
-                    right=mid-1
-            # print("After lr",left," ",right)
-            if left<=n:
-                globalcount+=(n-left)
-        return globalcount
         
+    
+        # Solution 2
+        # left=0
+        # for i in range(len(grid)-1,-1,-1):
+        #     right=n-1
+        #     while(left<=right):
+        #         mid=left+(right-left)//2
+        #         if(grid[i][mid]>-1):
+        #             left=mid+1
+        #         else:
+        #             right=mid-1
+        #     if left<=n:
+        #         globalcount+=(n-left)
+        # return globalcount
+        
+            # def countNegatives(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        r, c, cnt = 0, n - 1, 0
+        while r < m and c >= 0:
+            if grid[r][c] < 0:
+                cnt += m - r
+                c -= 1
+            else:
+                r += 1
+        return cnt
